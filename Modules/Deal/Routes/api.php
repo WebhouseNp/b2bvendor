ecfd6b1a5ef480d06890f0662ebf6d30ab8157a1
manojbhatta <bob@example.com>
2022-01-03 10:41:17 +0545

    customer and product search api for deals

--- Modules/Deal/Routes/api.php
+++ Modules/Deal/Routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use Modules\Deal\Http\Controllers\DealApiController;
+use Modules\Deal\Http\Controllers\DealController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +21,7 @@ Route::middleware('auth:api')->get('/deal', function (Request $request) {
 Route::delete('/deals', [DealController::class,'destroy'])->name('api.deletedeal');
 Route::put('/deals', [DealController::class,'update'])->name('api.updatedeal');
 Route::post('/deals', [DealController::class,'store'])->name('api.storedeal');
+
+Route::get('deals/customer-search', [DealApiController::class,'customerSearch'])->name('api.productsearch');
+Route::get('deals/product-search', [DealApiController::class,'productSearch'])->name('api.productsearch');
 
