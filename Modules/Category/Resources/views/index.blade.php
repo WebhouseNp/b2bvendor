49b10cb317e4a6382ca8966fb64f1c4834262459
manojbhatta <bob@example.com>
2022-01-11 15:17:31 +0545

    bug fix

--- Modules/Category/Resources/views/index.blade.php
+++ Modules/Category/Resources/views/index.blade.php
@@ -22,7 +22,57 @@
 
         <div class="ibox-body" id="validation-errors">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close"> </div>
-        <div class="ibox-body" id="appendCategory"></div>
+        {{-- <div class="ibox-body" id="appendCategory"></div> --}}
+        <table class="table table-striped table-bordered table-hover" id="example-table" cellspacing="0" width="100%">
+            <thead>
+                <tr class="border-0">
+                    <th>S.N</th>
+                    <th>Name</td>
+                    <th>Image</td>
+                    <th>Include in main menu</td>
+                    <th>Hot Category</td>
+                    <th>Featured</td>
+                    <th>Contain Subcategory</td>
+                    <th>Publish</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody id="sortable">
+                @forelse ($details as $detail)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $detail->name }}</td>
+                    <td>
+                        @if($detail->image)
+                        <img class="img-fluid rounded" src="{{ $detail->imageUrl('thumbnail') }}" style="width: 3rem;">
+                        @else
+                        <p>N/A</p>
+                        @endif
+                    </td>
+                    <td>{{ $detail->include_in_main_menu == 1 ? 'Yes' : 'No' }}</td>
+                    <td>{{ $detail->hot_category == 1 ? 'Yes' : 'No' }}</td>
+                    <td>{{ $detail->is_featured == 1 ? 'Yes' : 'No' }}</td>
+                    <td>{{ $detail->does_contain_sub_category == 1 ? 'Yes' : 'No' }}</td>
+                    <td>{{ $detail->publish == 1 ? 'Published' : 'Not published' }}</td>
+                    <td class="text-nowrap">
+                        <a title="view" class="btn btn-success btn-sm" href="{{ route('category.view',$detail->id) }}">
+                            <i class="fa fa-eye"></i>
+                        </a>
+                        <a title="Edit" class="btn btn-primary btn-sm" href="{{ route('category.edit',$detail->id) }}">
+                            <i class="fa fa-edit"></i>
+                        </a>
+                        <button class="btn btn-danger btn-sm delete-category" onclick="return confirm('Do You want to delete this category??') && deleteCategory(this,'{{ $detail->id }}')"  class="btn btn-danger" style="display:inline"><i class="fa fa-trash"></i></button>
+                        <!-- <button type="button" class="btn btn-danger delete" onclick="deleteCategory({{ $detail->id }})" class="btn btn-danger" style="display: inline;" title="delete"><i class="fa fa-trash"></i></button> -->
+                    </td>
+                </tr>
+                @empty
+                <tr>
+                    <td colspan="42">No Categories found </td>
+                </tr>
+                @endforelse
+            </tbody>
+        </table>
+        
     </div>
 </div>
 @endsection
@@ -66,10 +116,11 @@
 
     // load the categories
     categories();
+});
 
-   function deleteCategory(el,id) {
-        let url = "/api/deletecategory/" + id;
-        $.ajax({ 
+function deleteCategory(el,id) {
+     let url = "/api/deletecategory/" + id;
+     $.ajax({ 
         type: "post", 
         url: url, 
         data:{
@@ -81,9 +132,8 @@
             $('#validation-errors').append('<div class="alert alert-success">'+validation_errors+'</div');
             $(el).closest('tr').remove()
         }
-        }); 
-    } 
-});
+     }); 
+ } 
 
   </script>
 @endsection
