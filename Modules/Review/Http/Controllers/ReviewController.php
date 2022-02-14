e1dfe4880b66867c8d9a3540a1b98a329863cdac
samikshyalamichhane <samikshya.lamichhane111@gmail.com>
2022-02-14 13:23:32 +0545

    review in product show api added

--- Modules/Review/Http/Controllers/ReviewController.php
+++ Modules/Review/Http/Controllers/ReviewController.php
@@ -7,17 +7,12 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Validator;
 use Modules\Review\Entities\Review;
+use Modules\Review\Transformers\ReviewCollection;
 
 class ReviewController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     * @return Renderable
-     */
-
     public function createReview(Request $request){
         try {
-            //code...
             $validator = Validator::make($request->all(), [
                 'name'             => 'required|string',
                 'reviews'          => 'required|string',
@@ -53,65 +48,9 @@ class ReviewController extends Controller
         return view('review::index',compact('reviews'));
     }
 
+    public function productReview($id){
+        $reviews = Review::where('product_id', $id)->with('user','product')->get();
+        return new ReviewCollection($reviews);
+    }
    
-
-    /**
-     * Show the form for creating a new resource.
-     * @return Renderable
-     */
-    public function create()
-    {
-        return view('review::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Renderable
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function show($id)
-    {
-        return view('review::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function edit($id)
-    {
-        return view('review::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Renderable
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
