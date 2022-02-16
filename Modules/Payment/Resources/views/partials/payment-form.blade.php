fd42ab133fc3532cf45dafe3d9ae86b563cd1fed
James Bhatta <jmsbhatta@gmail.com>
2022-02-16 11:36:46 +0545

    payment issue fix

--- Modules/Payment/Resources/views/partials/payment-form.blade.php
+++ Modules/Payment/Resources/views/partials/payment-form.blade.php
@@ -27,7 +27,7 @@
                         </div>
                         <div class="form-group">
                             <label for="" class="required">Attachment</label>
-                            <input type="file" name="attachment" class="form-control">
+                            <input type="file" name="file" class="form-control">
                         </div>
                         <div class="form-group">
                             <button type="submit" class="btn btn-success">Deposit</button>
