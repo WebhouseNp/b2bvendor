fa16e788f83f171ee8d24a7b793ea0b9c895476b
James Bhatta <jmsbhatta@gmail.com>
2022-02-23 10:11:42 +0545

    wip

--- Modules/Product/Database/Migrations/2021_07_09_102456_create_products_table.php
+++ Modules/Product/Database/Migrations/2021_07_09_102456_create_products_table.php
@@ -18,24 +18,11 @@ class CreateProductsTable extends Migration
             $table->string('title');
             $table->string('slug')->unique();
             $table->unsignedBigInteger('product_category_id')->nullable();
-            $table->double('shipping_charge', 15, 8)->nullable();
+            $table->double('shipping_charge', 15, 2)->nullable();
             $table->text('highlight')->nullable();
             $table->text('description')->nullable();
             $table->string('video_link')->nullable();
-            // $table->unsignedBigInteger('category_id')->nullable();
-            // $table->unsignedBigInteger('subcategory_id')->nullable();
-            // $table->unsignedBigInteger('offer_id')->nullable();
-            // $table->unsignedBigInteger('brand_id')->nullable();
-            // $table->float('discount')->nullable();
-            // $table->double('price', 15, 8)->nullable();
-            // $table->unsignedInteger('moq')->nullable();
-            // $table->unsignedInteger('delivery_charge')->nullable();
-            // $table->boolean('essential', [0, 1])->default(0);
-            // $table->boolean('best_seller', [0, 1])->default(0);
             $table->string('image')->nullable();
-            // $table->string('quantity')->nullable();
-            $table->text('non_approval_note')->nullable();
-            // $table->enum('status', ['active', 'inactive'])->default('active');
             $table->boolean('status')->default(true);          
             $table->boolean('is_top')->default(false);
             $table->boolean('is_new_arrival')->default(false);
@@ -51,9 +38,6 @@ class CreateProductsTable extends Migration
             $table->unsignedBigInteger('user_id')->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('SET NULL');
             $table->foreign('product_category_id')->references('id')->on('product_categories')->onDelete('CASCADE');
-            // $table->foreign('subcategory_id')->references('id')->on('subcategories')->onDelete('CASCADE');
-            // $table->foreign('offer_id')->references('id')->on('offers')->onDelete('CASCADE');
-            // $table->foreign('brand_id')->references('id')->on('brands')->onDelete('CASCADE');
 
             $table->softDeletes();
             $table->timestamps();
