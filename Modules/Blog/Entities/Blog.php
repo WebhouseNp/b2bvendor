12294a19d733976eb6456982a296fac1cdea6319
samikshyalamichhane <samikshya.lamichhane111@gmail.com>
2022-02-25 17:45:17 +0545

    blog and faq crud updated

---
+++ Modules/Blog/Entities/Blog.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Modules\Blog\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Support\Facades\Storage;
+
+
+class Blog extends Model
+{
+
+    use Sluggable;
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title',
+                'separator' => '_'
+            ]
+        ];
+    }
+
+    public function imageUrl()
+    {
+        return Storage::disk('public')->url($this->image);
+    }
+
+    public function deleteImage()
+    {
+        return Storage::delete($this->image);
+    }
+    
+}
--- Modules/Order/Routes/api.php
+++ Modules/Order/Routes/api.php
@@ -2,19 +2,9 @@
 
 use Illuminate\Http\Request;
 
-// Route::middleware('auth:api')->get('/order', function (Request $request) {
-//     return $request->user();
-// });
 
 Route::post('/createorder', 'OrderController@createorder');
 Route::get('/getorders', 'OrderController@getorders');
 Route::post('/changeOrderStatus', 'OrderController@changeOrderStatus')->name('api.changeOrderStatus');
 Route::get('/editorder', 'OrderController@editorder')->name('editorder');
 
-Route::group(['middleware' => ['auth:api', 'Adminrole']], function () {
-    Route::get('/get-vendor-orders', 'OrderController@getVendorOrders')->name('getVendorOrders');
-    // // Route::post('/getroleFromID', 'RoleController@getroleFromID');
-    // Route::get('/view-role', 'RoleController@viewRole')->name('viewRole');
-    // Route::get('/editrole', 'RoleController@editRole')->name('editRole');
-    // Route::post('/updaterole', 'RoleController@updateRole');
-});
