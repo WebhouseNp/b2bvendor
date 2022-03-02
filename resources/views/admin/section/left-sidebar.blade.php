1c5f4732a90748f05bce503f65efcb350861675d
James Bhatta <jmsbhatta@gmail.com>
2022-03-02 16:41:31 +0545

    test notification

--- resources/views/admin/section/left-sidebar.blade.php
+++ resources/views/admin/section/left-sidebar.blade.php
@@ -406,15 +406,6 @@ $user = auth()->user();
             </li>
             @endif
 
-            @if(auth()->user()->hasAnyRole(['super_admin', 'admin']))
-            <li>
-                <a href="{{ route('settings.sastowholesale-mall.index') }}">
-                    <i class="sidebar-item-icon fa fa-thumbs-up"></i>
-                    <span class="nav-label">Sasto Wholesale Mall</span>
-                </a>
-            </li>
-            @endif
-
             @if(auth()->user()->hasAnyRole('super_admin|admin'))
             <li>
                 <a href="javascript:;">
@@ -512,6 +503,32 @@ $user = auth()->user();
                             All Lists
                         </a>
                     </li>
+                </ul>
+            </li>
+            @endif
+
+            @if(auth()->user()->hasAnyRole('super_admin|admin'))
+            <li>
+                <a href="javascript:;">
+                    <i class="sidebar-item-icon fa fa-adn"></i>
+                    <span class="nav-label">Settings</span>
+                    <i class="fa fa-angle-left arrow"></i>
+                </a>
+                <ul class="nav-2-level collapse">
+                    <li>
+                        <a href="{{ route('settings.sastowholesale-mall.index') }}">
+                            <span class="fa fa-circle-o"></span>
+                            Sasto Wholesale Mall
+                        </a>
+                    </li>
+                    @if(auth()->user()->hasAnyRole('super_admin'))
+                    <li>
+                        <a href="{{ route('settings.notification.index') }}">
+                            <span class="fa fa-circle-o"></span>
+                            <span>Test Notifications</span>
+                        </a>
+                    </li>
+                    @endif
                 </ul>
             </li>
             @endif
