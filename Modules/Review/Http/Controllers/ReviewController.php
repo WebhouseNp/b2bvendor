77f8bcb7fdae8e1a83c3b97c2881f4c45930bc87
samikshyalamichhane <samikshya.lamichhane111@gmail.com>
2022-03-03 14:10:42 +0545

    updated

--- Modules/Review/Http/Controllers/ReviewController.php
+++ Modules/Review/Http/Controllers/ReviewController.php
@@ -39,62 +39,18 @@ class ReviewController extends Controller
     public function createReview(Request $request)
     {
         try {
-            $validator = Validator::make($request->all(), [
+            $request->validate([
                 'name'             => 'required|string',
                 'reviews'          => 'required|string',
                 'customer_id'       => 'required|numeric|exists:users,id',
                 'product_id'       => 'required|numeric|exists:products,id',
                 'rate'             => 'required|numeric'
-            ]);
-            if ($validator->fails()) {
-                return response()->json(['status' => 'unsuccessful', 'data' => $validator->messages()], 422);
-            }
+              ]);
 
             if (!Review::canReview($request->product_id, $request->customer_id)) {
-                return response()->json(['status' => 'unsuccessful', 'data' => 'Sorry you cannot review this product.'], 422);
+                return response()->json(['status' => 'unsuccessful', 'data' => 'Sorry you cannot review this product.']);
             }
 
-            $formData = $request->all();
-            $reviews = DB::table('reviews')
-                ->where('product_id', $request->product_id)
-                ->where('customer_id', $request->customer_id)
-                ->get();
-            if ($reviews->isNotEmpty()) {
-                return response()->json([
-                    "message" => "You have already given review to the product!!"
-                ], 200);
-            } else {
-                $data = Review::create($formData);
-                return response()->json([
-                    "message" => "Review created!!",
-                    "data" => $data
-                ], 200);
-            }
-            // $order_list = Order::with('orderList')
-            //     ->whereHas('orderList', function (Builder $query) use ($request) {
-            //         $query->where('product_id', $request->product_id);
-            //     })
-            //     ->get();
-            // $reviews = DB::table('reviews')
-            //     ->where('product_id', $request->product_id)
-            //     ->where('customer_id', $request->customer_id)
-            //     ->get();
-            // if ($reviews->isNotEmpty()) {
-            //     return response()->json([
-            //         "message" => "You have already given review to the product!!"
-            //     ], 200);
-            // }
-            // if ($order_list->isEmpty()) {
-            //     return response()->json([
-            //         "message" => "Review cannot be created as you have not brought the product!!"
-            //     ], 200);
-            // } else if ($order_list->isNotEmpty() && $reviews->isEmpty()) {
-            //     $data = Review::create($formData);
-            //     return response()->json([
-            //         "message" => "Review created!!",
-            //         "data" => $data
-            //     ], 200);
-            // }
         } catch (\Exception $ex) {
             Log::error('Review Created', [
                 'status' => '500',
