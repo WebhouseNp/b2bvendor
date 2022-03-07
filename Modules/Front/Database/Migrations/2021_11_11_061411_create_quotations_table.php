d0bd8e00c5a59e54bfb65f254d591b66824ab994
James Bhatta <jmsbhatta@gmail.com>
2022-03-07 16:58:06 +0545

    rfq wip

--- Modules/Front/Database/Migrations/2021_11_11_061411_create_quotations_table.php
+++ Modules/Front/Database/Migrations/2021_11_11_061411_create_quotations_table.php
@@ -15,22 +15,22 @@ class CreateQuotationsTable extends Migration
     {
         Schema::create('quotations', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name')->nullable();
-            $table->string('last_name')->nullable();
-            $table->string('email')->nullable();
             $table->string('purchase')->nullable();
             $table->string('quantity')->nullable();
+            $table->string('unit')->nullable();
+            $table->text('specifications')->nullable();
             $table->string('image1')->nullable();
             $table->string('image2')->nullable();
             $table->string('image3')->nullable();
-            $table->string('contact_num')->nullable();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
             $table->string('mobile_num')->nullable();
+            $table->string('whatsapp')->nullable();
             $table->string('ship_to')->nullable();
             $table->string('expected_price')->nullable();
             $table->string('expected_del_time')->nullable();
             $table->string('other_contact')->nullable();
             $table->string('link')->nullable();
-            $table->text('specifications')->nullable();
 
             $table->timestamps();
         });
--- Modules/Front/Http/Controllers/QuotationController.php
+++ Modules/Front/Http/Controllers/QuotationController.php
@@ -18,17 +18,8 @@ class QuotationController extends Controller
      */
     public function index()
     {
-        $quotations = Quotation::orderBy('created_at','Desc')->get();
-        return view('front::index',compact('quotations'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     * @return Renderable
-     */
-    public function create()
-    {
-        return view('front::create');
+        $quotations = Quotation::orderBy('created_at', 'Desc')->get();
+        return view('front::index', compact('quotations'));
     }
 
     /**
@@ -38,132 +29,57 @@ class QuotationController extends Controller
      */
     public function store(Request $request)
     {
-        // dd($request->all());
+        $request->validate([
+            'purchase'      => 'required|string',
+            'quantity'      => 'required|numeric',
+            'unit'      => 'required',
+            "specifications"  => "required",
+            'image1' =>  'nullable|mimes:jpg,jpeg,png|max:3000',
+            'image2' =>  'nullable|mimes:jpg,jpeg,png|max:3000',
+            'image3' =>  'nullable|mimes:jpg,jpeg,png|max:3000',
+            'name'    => 'required|string',
+            'email'         => 'required|email',
+            'mobile_num'    => 'required',
+            'ship_to'       => 'required',
+            'expected_price'  => 'required',
+            'expected_del_time' => 'required',
+            'other_contact' => 'required',
+            "link"  => "required",
+        ]);
+
         try {
-            $validator = Validator::make($request->all(), [
-                'first_name'             => 'required|string',
-                'last_name'             => 'required|string',
-                'email'             => 'required|email',
-                'purchase'             => 'required|string',
-                'quantity'          => 'required|numeric',
-                'contact_num'              => 'required',
-                'mobile_num'       => 'required',
-                'ship_to'        => 'nullable',
-                'expected_price'  => 'nullable',
-                'expected_del_time'           => 'nullable',
-                'other_contact'    => 'nullable',
-                "link"    => "required|link",
-                "specifications"  => "nullable",
-                'image1' =>  'nullable|mimes:jpg,jpeg,png|max:3000',
-                'image2' =>  'nullable|mimes:jpg,jpeg,png|max:3000',
-                'image3' =>  'nullable|mimes:jpg,jpeg,png|max:3000',
-
-            ]);
-            if ($validator->fails()) {
-                return response()->json(['status' => 'unsuccessful', 'data' => $validator->messages()],422);
-                exit;
-            }
-
-            $value = $request->except('image1','image2','image3');
-
-            if ($request->image1) {
-                $image = $this->imageProcessing('img-', $request->file('image1'));
-                $value['image1'] = $image;
-            }
-            if ($request->image2) {
-                $image = $this->imageProcessing('img-', $request->file('image2'));
-                $value['image2'] = $image;
-            }
-            if ($request->image3) {
-                $image = $this->imageProcessing('img-', $request->file('image3'));
-                $value['image3'] = $image;
-            }
-            $data = Quotation::create($value);
-            $quotation_info = [
+            $quotation = new Quotation([
+                'purchase' => $request->purchase,
+                'quantity' => $request->quantity,
+                'unit' => $request->unit,
+                "specifications" => $request->specifications,
+                'image1' => $request->hasFile('image1') ? $request->file('image1')->store('uploads/quotations') : null,
+                'image2' => $request->hasFile('image2') ? $request->file('image2')->store('uploads/quotations') : null,
+                'image3' => $request->hasFile('image3') ? $request->file('image3')->store('uploads/quotations') : null,
                 'name' => $request->first_name,
                 'email' => $request->email,
-                'purchase' =>$request->purchase,
-                'quantity' => $request->quantity,
-                'contact_num'              => $request->contact_num,
-                'mobile_num'       => $request->mobile_num,
-                'ship_to'        => $request->ship_to,
+                'mobile_num'    => $request->mobile_num,
+                'ship_to'       => $request->ship_to,
                 'expected_price'  => $request->expected_price,
-                'expected_del_time'           => $request->expected_del_time,
-                'other_contact'    => $request->other_contact,
-                "link"    => $request->link,
-                "specifications" => $request->specifications
-            ];
-                Mail::send('email.quotation',$quotation_info,  function ($message) use ($request){
-                    $message->to('info@sastowholesale.com','Admin')
-    
-                        ->subject('Quotation Received' );
-                    $message->from($request->email, $request->first_name);
-                });
-            return response()->json(['status' => 'successful', 'message' => 'Quotation Submitted successfully.', 'data' => $data]);
+                'expected_del_time' => $request->expected_del_time,
+                'other_contact' => $request->other_contact,
+                "link"  => $request->link,
+            ]);
+
+            $quotation->save();
+
+            // Mail::send('email.quotation', $quotation,  function ($message) use ($request) {
+            //     $message->to('info@sastowholesale.com', 'Admin')
+            //         ->subject('Quotation Received');
+            //     $message->from($request->email, $request->first_name);
+            // });
+
+            return response()->json(['status' => 'successful', 'message' => 'Quotation Submitted successfully.', 'data' => $quotation]);
         } catch (\Exception $exception) {
             DB::rollback();
             return response([
                 'message' => $exception->getMessage()
             ], 400);
         }
-    }
-
-    public function imageProcessing($type, $image)
-    {
-        $input['imagename'] = $type . time() . '.' . $image->getClientOriginalExtension();
-        $thumbPath = public_path('images/thumbnail');
-        $mainPath = public_path('images/main');
-        $listingPath = public_path('images/listing');
-
-        $img1 = Image::make($image->getRealPath());
-        $img1->fit(530, 300)->save($thumbPath . '/' . $input['imagename']);
-
-
-        $img2 = Image::make($image->getRealPath());
-        $img2->fit(99, 88)->save($listingPath . '/' . $input['imagename']);
-
-        $destinationPath = public_path('/images');
-        return $input['imagename'];
-    }
-
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function show($id)
-    {
-        return view('front::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function edit($id)
-    {
-        return view('front::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Renderable
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
