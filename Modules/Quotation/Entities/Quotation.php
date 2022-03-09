e068c7bfdb36e3dc3c0ebccace0c35deed6a4383
James Bhatta <jmsbhatta@gmail.com>
2022-03-09 15:37:27 +0545

    quotation with replies

--- Modules/Quotation/Entities/Quotation.php
+++ Modules/Quotation/Entities/Quotation.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Quotation\Entities;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Modules\User\Entities\Vendor;
@@ -16,9 +17,19 @@ class Quotation extends Model
     {
         return \Modules\Quotation\Database\factories\QuotationFactory::new();
     }
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function vendors()
     {
         return $this->belongsToMany(Vendor::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(QuotationReply::class, 'quotation_id')->latest();
     }
 }
