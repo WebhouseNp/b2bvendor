832809b36bcc2446440ccb51d8d94f7e0482919c
samikshyalamichhane <samikshya.lamichhane111@gmail.com>
2022-03-11 17:42:05 +0545

    sms token update

---
+++ Modules/Front/Notifications/VendorStatusChangeMessageNotification.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Modules\Front\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class VendorStatusChangeMessageNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['smsapi'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toSmsApi($notifiable)
+    {
+        return [
+            'message' => 'Your venfor has been (#' . $this->user->vendor_status . ').',
+        ];
+        
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
