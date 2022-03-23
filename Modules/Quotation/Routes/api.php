4b57600ae959435e4cee0a5dbce0ca449f5d9a06
James Bhatta <jmsbhatta@gmail.com>
2022-03-24 00:01:49 +0545

    removed duplication quotation route name

--- Modules/Quotation/Routes/api.php
+++ Modules/Quotation/Routes/api.php
@@ -5,7 +5,7 @@ use Modules\Quotation\Http\Controllers\QuotationApiController;
 use Modules\Quotation\Http\Controllers\QuotationController;
 
 Route::group(['middleware' => 'auth:api'],  function () {
-    Route::get('quotations', [QuotationApiController::class, 'index'])->name('quotations.index');
-    Route::get('quotations/{quotation}', [QuotationApiController::class, 'show'])->name('quotations.show');
+    Route::get('quotations', [QuotationApiController::class, 'index']);
+    Route::get('quotations/{quotation}', [QuotationApiController::class, 'show']);
     Route::post('quotation', [QuotationController::class, 'store'])->name('quotations.store');
 });
