799959cb1d6e1380e406d64c0b7d5eddd697cff4
James Bhatta <jmsbhatta@gmail.com>
2022-04-05 17:23:36 +0545

    take social redirect url from env

--- config/constants.php
+++ config/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 return  [
-    'customer_app_url' => env('CUSTOMER_APP_URL', 'https://staging.sastowholesale.com/'),
+    'customer_app_url' => env('CUSTOMER_APP_URL', 'https://sastowholesale.com/'),
 
     'order_statuses' => ['pending', 'processing', 'shipped', 'completed', 'cancelled', 'refunded'],
     'package_statuses' => ['pending', 'processing', 'shipped', 'completed', 'cancelled', 'refunded'], // Not in use
