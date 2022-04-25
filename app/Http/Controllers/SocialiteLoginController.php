08b6921dc22787dd38df52ff7f371179341ddf06
James Bhatta <jmsbhatta@gmail.com>
2022-04-25 16:47:48 +0545

    log the user from facebook login

--- app/Http/Controllers/SocialiteLoginController.php
+++ app/Http/Controllers/SocialiteLoginController.php
@@ -96,6 +96,7 @@ class SocialiteLoginController extends Controller
     public function handleFacebookCallBack(){
         try{
             $user = Socialite::driver(static::FACEBOOK_TYPE)->stateless()->user();
+            logger($user);
             if($user->email === null){
                 return response()->json([
                     "status" => "false",
