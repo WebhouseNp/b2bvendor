74ad5b64de1b3b713eeed06b526642b035c09106
Hem Raj Gurung <hemrajgrg9876@gmail.com>
2022-04-26 11:08:10 +0430

    sdc

--- config/services.php
+++ config/services.php
@@ -33,13 +33,13 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('FRONT_APP').'/login/google/callback',
+        'redirect' => env('CUSTOMER_APP_URL', 'https://sastowholesale.com').'/login/google/callback',
     ],
 
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => env('FRONT_APP').'/login/facebook/callback',
+        'redirect' => env('CUSTOMER_APP_URL', 'https://sastowholesale.com').'/login/facebook/callback',
     ],
 
     'sms_api' => [
