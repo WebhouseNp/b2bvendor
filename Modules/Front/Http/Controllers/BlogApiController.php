97c33c66666889608750cc03153995a2435de979
James Bhatta <jmsbhatta@gmail.com>
2022-05-16 14:56:09 +0545

    add limit to blog api

--- Modules/Front/Http/Controllers/BlogApiController.php
+++ Modules/Front/Http/Controllers/BlogApiController.php
@@ -2,28 +2,29 @@
 
 namespace Modules\Front\Http\Controllers;
 
-use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Modules\Blog\Entities\Blog;
-use Modules\Blog\Transformers\BlogCollection;
 use Modules\Blog\Transformers\BlogResource;
 
 class BlogApiController extends Controller
 {
     public function index()
     {
-        $blogs = Blog::published()->get();
+        $blogs = Blog::published()
+            ->when(request()->filled('limit'), function ($query) {
+                $query->limit(request('limit'));
+            })
+            ->latest()
+            ->get();
+
         return BlogResource::collection($blogs)->hide([
             'description',
             'id'
         ]);
-        // return new BlogCollection($blogs);
     }
 
     public function show(Blog $blog)
     {
         return new BlogResource($blog);
     }
-
 }
